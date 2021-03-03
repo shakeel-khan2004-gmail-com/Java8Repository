c95ceab7ce818ab25506b2bb78c3d9deefd91940
shakeel <mailtoshakeel786@gmail.com>
2021-03-03 14:45:16 +0530

    commit by branch1

--- JavaTest1.8/src/BranchTest.java
+++ JavaTest1.8/src/BranchTest.java
@@ -2,6 +2,7 @@ public class BranchTest {
 	public static void main(String[] args) {
 System.out.println("Test my branch");
 System.out.println("Test my branch1");
+System.out.println("conflict");
 	}
 
 }
