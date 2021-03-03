67379362391e4ca070b5ded9bec5d56ade561ac2
shakeel <mailtoshakeel786@gmail.com>
2021-03-03 17:41:14 +0530

    commited

--- JavaTest1.8/src/BranchTest.java
+++ JavaTest1.8/src/BranchTest.java
@@ -6,6 +6,8 @@ System.out.println("Test my branch2");
 System.out.println("Hi I am here ");
 System.out.println("conflict");
 System.out.println("conflict2");
+System.out.println("Hi I am here ");
+
 	}
 
 }
