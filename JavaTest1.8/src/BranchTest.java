1b43bf1ac1a5436b0dce7dea56441d0e7721d153
shakeel <mailtoshakeel786@gmail.com>
2021-03-05 11:00:34 +0530

    commited

--- JavaTest1.8/src/BranchTest.java
+++ JavaTest1.8/src/BranchTest.java
@@ -1,12 +1,8 @@
 public class BranchTest {
 	public static void main(String[] args) {
-System.out.println("Test my branch");
-System.out.println("Test my branch1");
-System.out.println("Test my branch2");
-System.out.println("Hi I am here ");
-System.out.println("conflict");
-System.out.println("conflict2");
-System.out.println("Hi I am here ");
+		System.out.println("Test my branch");
+		System.out.println("conflict");
+		System.out.println("Hi I am here ");
 	}
 
 }
